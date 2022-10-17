9bb237bd9ec748233fa42c472cc1053731befec9
Ivan <revillaivan97@gmail.com>
2022-10-16 18:10:44 -0700

    added main file

---
+++ yelp_roulette_artifact/src/main/java/com/yelproulette/yelp_roulette_artifact/YelpRouletteArtifactApplication.java
@@ -0,0 +1,13 @@
+package com.yelproulette.yelp_roulette_artifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YelpRouletteArtifactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YelpRouletteArtifactApplication.class, args);
+	}
+
+}
---
+++ yelp_roulette_artifact/src/test/java/com/yelproulette/yelp_roulette_artifact/YelpRouletteArtifactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yelproulette.yelp_roulette_artifact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YelpRouletteArtifactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
