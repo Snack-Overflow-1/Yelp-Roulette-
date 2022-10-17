6fdf1121a72f5da4bd7d2a3c8640207d36eeaa94
Ivan <revillaivan97@gmail.com>
2022-10-16 19:24:28 -0700

    adding/pulling from mongo works now

--- yelp_roulette_artifact/src/main/java/com/yelproulette/yelp_roulette_artifact/MongoDB.java
+++ yelp_roulette_artifact/src/main/java/com/yelproulette/yelp_roulette_artifact/MongoDB.java
@@ -1,7 +1,12 @@
 package com.yelproulette.yelp_roulette_artifact;
 
 import com.mongodb.*;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
 import java.net.UnknownHostException;
+
+import org.bson.Document;
 
 public class MongoDB {
 
@@ -10,25 +15,18 @@ public class MongoDB {
     public static DBCollection test;
 
     public static void main(String[] args) {
-        mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
-        database = mongoClient.getDB("Database1");
-        test = database.getCollection("test");
+        String uri = "mongodb+srv://dbtest:abcd1234@cluster0.elfhy1n.mongodb.net/test";
+        MongoClientURI clienturi = new MongoClientURI(uri);
+        MongoClient mongoClient = new MongoClient(clienturi);
 
-        TestObj testObj = new TestObj();
-        testObj.setMemberID("abcd");
-        testObj.setTimer(1);
-        testObj.setX(100);
+        MongoDatabase mongoDatabase = mongoClient.getDatabase("mongoclientdb");
+        MongoCollection collection = mongoDatabase.getCollection("test");
 
-        test.insert(convert(testObj));
+        Document document = new Document("name", "Ivan");
+        document.append("Sex", "male");
+        document.append("age", "3");
+        document.append("race", "butterfly");
 
-        DBObject query = new BasicDBObject("Timer:", 1);
-        DBCursor cursor = test.find(query);
-        System.out.println(cursor.one());
+        collection.insertOne(document);
     }
-
-    public static DBObject convert(TestObj testObj) {
-        return new BasicDBObject("x:", testObj.getX()).append("Timer:", testObj.getTimer()).append("memberID:",
-                testObj.getMemberID());
-    }
-
 }
