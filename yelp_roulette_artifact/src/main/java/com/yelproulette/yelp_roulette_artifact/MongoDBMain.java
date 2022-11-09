6ecedfa0328a0839730417cbd252909e4d5ae8d4
Ivan <revillaivan97@gmail.com>
2022-11-08 21:57:30 -0800

    Added more methods to make it easier to add and search the database

--- yelp_roulette_artifact/src/main/java/com/yelproulette/yelp_roulette_artifact/MongoDBMain.java
+++ yelp_roulette_artifact/src/main/java/com/yelproulette/yelp_roulette_artifact/MongoDBMain.java
@@ -14,56 +14,51 @@ public class MongoDBMain {
     public static DB database;
     public static DBCollection test;
     public static MongoCollection collection;
+    public final static String uri = "mongodb+srv://dbtest:abcd1234@cluster0.flvwqqy.mongodb.net/test";
 
     public static void main(String[] args) {
-        MongoDBMain mongodb = new MongoDBMain();
-        String uri = "mongodb+srv://dbtest:abcd1234@cluster0.flvwqqy.mongodb.net/test";
-        MongoClientURI clienturi = new MongoClientURI(uri);
-        mongoClient = new MongoClient(clienturi);
-        MongoDatabase mongoDatabase = mongoClient.getDatabase("mongoclientdb");
-        collection = mongoDatabase.getCollection("test");
+        try {
+            MongoClientURI clienturi = new MongoClientURI(uri);
+            mongoClient = new MongoClient(clienturi);
+            MongoDatabase mongoDatabase = mongoClient.getDatabase("mongoclientdb");
+            collection = mongoDatabase.getCollection("test");
 
-        Document document = new Document("name", "Kyle");
-        document.append("Sex", "male");
-        document.append("age", "24");
-        document.append("race", "Human");
+            MongoDBService mdbs = new MongoDBService();
+            mdbs.addUserToDatabase("Ivan", "Revilla", "email@email.com", 20);
 
-        // insert - add
-        // delete
-        // update
-
-        // collection.insertOne(document);
-        System.out.println("Database connected...");
-        System.out.println(mongodb.searchDocumentForUser("name", "Kyle"));
-    }
-
-    // ----------------------------FINDING A USER AND UPDATING THEIR
-    // DATA--------------------------
-    // Document search = new Document("name", "Ivan");
-    // Document found = (Document) collection.find(new Document("name",
-    // "Ivan")).first();
-
-    // if (found != null) {
-    // System.out.println("Found user...");
-
-    // Bson updatedValue = new Document("age", 5);
-    // Bson updateOperation = new Document("$set", updatedValue);
-    // collection.updateOne(found, updateOperation);
-    // System.out.println("User updated...");
-    // }
-    // ----------------------------END FINDING A USER AND UPDATING THEIR
-    // DATA-------------------------- }
-
-    public boolean searchDocumentForUser(String name, String nameValue) {
-        boolean foundUser = false;
-        Document found = (Document) collection.find(new Document(name,
-                nameValue)).first();
-
-        if (found != null) {
-            System.out.println("Found user...");
-            return foundUser = true;
+            System.out.println(mdbs.getUser("name", "Kyle"));
+        } catch (Exception e) {
+            System.out.println(e);
         }
-        System.out.println("User not found...");
-        return foundUser;
     }
+
 }
+
+// ----------FINDING A USER AND UPDATING THEIR DATA----------
+// Document search = new Document("name", "Ivan");
+// Document found = (Document) collection.find(new Document("name",
+// "Ivan")).first();
+
+// if (found != null) {
+// System.out.println("Found user...");
+
+// Bson updatedValue = new Document("age", 5);
+// Bson updateOperation = new Document("$set", updatedValue);
+// collection.updateOne(found, updateOperation);
+// System.out.println("User updated...");
+// }
+// ----------END FINDING A USER AND UPDATING THEIR DATA----------
+
+// ----------EXAMPLE ON HOW TO ADD ONTO MONGODB-----------
+// Document document = new Document("name", "Kyle");
+// document.append("Sex", "male");
+// document.append("age", "24");
+// document.append("race", "Human");
+// collection.insertOne(document);
+// ----------END EXAMPLE ON HOW TO ADD ONTO MONGODB-----------
+
+// ----------OTHER BUILT IN METHODS----------
+// collection.insert - add
+// collection.delete
+// collection.update
+// ----------END OTHER BUILT IN METHODS----------
--- yelp_roulette_artifact/src/main/java/com/yelproulette/yelp_roulette_artifact/MongoDBService.java
+++ yelp_roulette_artifact/src/main/java/com/yelproulette/yelp_roulette_artifact/MongoDBService.java
@@ -13,6 +13,9 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
 import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import java.net.UnknownHostException;
 
 public class MongoDBService {
 
@@ -20,20 +23,24 @@ public class MongoDBService {
     private static DB database;
     private static DBCollection test;
     private static MongoCollection collection;
+    // creating a cursor for searching through the database
+    private static DBCursor cursor;
 
     /**
      * This method starts the client
      */
     public static void runTime() {
         // access the mongo database and create a MongoClient object from uri
-        String uri = "mongodb+srv://dbtest:abcd1234@cluster0.elfhy1n.mongodb.net/test";
+        // String uri =
+        // "mongodb+srv://dbtest:abcd1234@cluster0.elfhy1n.mongodb.net/test";
+        String uri = "mongodb+srv://dbtest:abcd1234@cluster0.flvwqqy.mongodb.net/test";
         MongoClientURI clienturi = new MongoClientURI(uri);
         mongoClient = new MongoClient(clienturi);
 
         // get the database and collection
         MongoDatabase mongoDatabase = mongoClient.getDatabase("mongoclientdb");
         collection = mongoDatabase.getCollection("test");
-
+        System.out.println("Database connected...");
     }
 
     /**
@@ -58,6 +65,25 @@ public class MongoDBService {
     }
 
     /**
+     * This method returns the Document of a user from a specfic title and value
+     * within the MongoDB Collection
+     * 
+     * @param title      String of the title
+     * @param titleValue String value of the given title
+     * @return BSON format of the information from that Document that was found
+     */
+    public Document getUserWithString(String title, String titleValue) {
+        Document found = null;
+        boolean bool = searchDocumentForUser(title, titleValue);
+        if (bool == true) {
+            Document search = new Document(title, titleValue);
+            found = (Document) collection.find(search).first();
+            return found;
+        }
+        return found;
+    }
+
+    /**
      * This method returns true if a user with a specific title and its value are
      * found... false otherwise
      * 
@@ -76,6 +102,28 @@ public class MongoDBService {
         }
         System.out.println("User not found...");
         return foundUser;
+    }
+
+    /**
+     * This method adds a user with given paramaters as data into MongoDB. It
+     * appends all data parameters... connects to the database with the runtime...
+     * prints to console that it is connected... then uploads the parameters to
+     * MongoDB
+     * 
+     * @param firstName String of the name of the user we want to add
+     * @param lastName  String of the last name of the user we want to add
+     * @param email     String of the user's email
+     * @param age       int of the age of the user
+     */
+    public void addUserToDatabase(String newFirstName, String newLastName, String newEmail, int newAge) {
+        Document newDocument = new Document("First name", newFirstName);
+        newDocument.append("Last name", newLastName);
+        newDocument.append("Email", newEmail);
+        newDocument.append("Age", newAge);
+        runTime();
+        System.out.println("Database connected...");
+        collection.insertOne(newDocument);
+        System.out.println("Added " + newFirstName + " " + newLastName + " into MongoDB....");
     }
 
 }
